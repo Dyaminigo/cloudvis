9fc378083370211154e9e9c659dda633add34c40
Daniil Lepke <McDanilRus@yandex.ru>
2022-01-18 12:43:22 +0700

    Global code refactoring and added missing API parameters to fully comply with API V2.

--- cloudvis.go
+++ cloudvis.go
@@ -1,98 +1,145 @@
 package main
-import "time"
-import "fmt"
-import "net/http"
-import "encoding/json"
-import "mime/multipart"
-import "io/ioutil"
-import "os"
-import "log"
-import "flag"
-import "bytes"
-var ApiUrl string = "https://visionbot.ru/apiv2"
-func multipartuploadreq(url string, params map[string]string, filedata []byte) *http.Request {
-rbody := new(bytes.Buffer)
-writer := multipart.NewWriter(rbody)
-filepart,_ := writer.CreateFormFile("file", "img")
-filepart.Write(filedata)
-for parname,parval := range params {
-writer.WriteField(parname, parval)
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"os"
+	"time"
+
+	requests "github.com/carlmjohnson/requests"
+)
+
+type UploadResponse struct {
+	Status string `json:"status"`
+	Id     string `json:"id"`
 }
-err := writer.Close()
-if err != nil {
-log.Fatal(err)
+
+type RecognitionResult struct {
+	UploadResponse
+	Text string `json:"text"`
+	QR   string `json:"qr"`
 }
-req,_ := http.NewRequest("POST", "https://visionbot.ru/apiv2/in.php", rbody)
-req.Header.Set("Content-Type", writer.FormDataContentType())
-return req
+
+func uploadImage(url string, params map[string]string, fileData []byte) (*UploadResponse, error) {
+	var upResponse UploadResponse
+
+	rbody := new(bytes.Buffer)
+	writer := multipart.NewWriter(rbody)
+	defer writer.Close()
+	filepart, _ := writer.CreateFormFile("file", "img")
+	filepart.Write(fileData)
+	for pname, pval := range params {
+		writer.WriteField(pname, pval)
+	}
+
+	err := requests.
+		URL(url).
+		BodyBytes(rbody.Bytes()).
+		ContentType(writer.FormDataContentType()).
+		ToJSON(&upResponse).
+		Fetch(context.Background())
+
+	return &upResponse, err
 }
+
+func getResult(url string, id string) (*RecognitionResult, error) {
+	var result RecognitionResult
+
+	err := requests.
+		URL(url).
+		Param("id", id).
+		ToJSON(&result).
+		Fetch(context.Background())
+
+	return &result, err
+}
+
 func main() {
-flag.Usage = func() {
-fmt.Fprintf(os.Stderr, "Usage: %s [opts] [file] \n file is a image file name. png and jpg are officially supported. \n Opts can be one of:\n", os.Args[0])
-flag.PrintDefaults()
-}
-usestdin := flag.Bool("stdin", false, "get image data from stdin, filename will be ignored")
-lang := flag.String("l", "en", "set `language` name to use. language is formed as two letter code, like ru or en, default is en.")
-target := flag.String("t", "all", "data `type` to recognize. can be 'text', 'image' or 'all'.")
-flag.Parse()
-var file *os.File
-var err error
-if *usestdin {
-file = os.Stdin
-} else {
-file,err = os.Open(flag.Arg(0))
-}
-if err != nil {
-log.Fatal(err)
-}
-filedata,err := ioutil.ReadAll(file)
-if err != nil {
-log.Fatal(err)
-}
-file.Close()
-params := map[string]string{"lang": *lang, "target": *target}
-req := multipartuploadreq(ApiUrl + "/in.php", params, filedata)
-os.Stderr.WriteString("uploading image\n")
-resp,err := http.DefaultClient.Do(req)
-if err != nil {
-log.Fatal(err)
-}
-bodydata,err := ioutil.ReadAll(resp.Body)
-if err != nil {
-log.Fatal(err)
-}
-resp.Body.Close()
-var bodyresult map[string]string
-err = json.Unmarshal(bodydata, &bodyresult)
-if err != nil {
-log.Fatal(err)
-}
-if bodyresult["status"] != "ok" {
-log.Fatal(fmt.Errorf("server error"))
-}
-id := bodyresult["id"]
-os.Stderr.WriteString("waiting for result\n")
-for {
-time.Sleep(500 * time.Millisecond)
-gresp,err := http.Get(ApiUrl + "/res.php?id=" + id)
-if err != nil {
-log.Fatal(err)
-}
-bodydata,err = ioutil.ReadAll(gresp.Body)
-if err != nil {
-log.Fatal(err)
-}
-bodyresult = nil
-err = json.Unmarshal(bodydata, &bodyresult)
-if err != nil {
-log.Fatal(err)
-}
-if bodyresult["status"] == "error" {
-log.Fatal(fmt.Errorf("server error"))
-}
-if bodyresult["status"] == "ok" {
-fmt.Println(bodyresult["text"])
-break
-}
-}
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s [opts] [file] \n file is a image file name. png and jpg are officially supported. \n Opts can be one of:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	useStdin := flag.Bool("stdin", false, "Get image data from stdin, filename will be ignored")
+	lang := flag.String("l", "en", "Set `language` name to use. Language is formed as two letter code, like ru or en")
+	target := flag.String("t", "all", "Data `type` to recognize. Can be 'text', 'image' or 'all'")
+	translate := flag.Bool("tr", false, "Translate recognition result")
+	qr := flag.Bool("qr", false, "Recognize QR/BAR codes")
+	flag.Parse()
+
+	var file *os.File
+	if *useStdin {
+		file = os.Stdin
+	} else {
+		var filename = flag.Arg(0)
+		if filename == "" {
+			log.Fatal("File name not specified")
+		}
+		if _, err := os.Stat(filename); os.IsNotExist(err) {
+			log.Fatalf("File %v does not exist", filename)
+		}
+		var err error
+		file, err = os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	filedata, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	const apiUrl = "https://visionbot.ru/apiv2"
+	params := map[string]string{
+		"lang":      *lang,
+		"target":    *target,
+		"qr":        "0",
+		"translate": "0",
+	}
+
+	if *qr {
+		params["qr"] = "1"
+	}
+	if *translate {
+		params["translate"] = "1"
+	}
+	fmt.Println("Uploading image...")
+	resp, err := uploadImage(apiUrl+"/in.php", params, filedata)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if resp.Status != "ok" {
+		log.Fatal("Server error. Try again later")
+	}
+
+	id := resp.Id
+	fmt.Println("Waiting for result...")
+	for {
+		res, err := getResult(apiUrl+"/res.php", id)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if res.Status == "error" {
+			log.Fatal("Server error. Try again later")
+		} else if res.Status == "ok" {
+			if res.Text == "" {
+				fmt.Println("No text")
+			} else {
+				fmt.Println(res.Text)
+			}
+			if res.QR != "" {
+				fmt.Printf("\nQR:\n%v", res.QR)
+			}
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
 }
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/dyaminigo/cloudvis
+
+go 1.17
+
+require (
+	github.com/carlmjohnson/requests v0.22.1 // indirect
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+)
